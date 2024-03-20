fbfdf0651779b3c250331098857fb9a63170cdb8
yhjang <jyh1203@naver.com>
2024-03-20 13:50:13 +0900

    commitsa

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,5 +1,12 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+
+
+
+
+
+
+
 $(document).ready(function () {
     $("#map-image").on("click")
     {
